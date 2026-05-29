4018a4ed9bc30f5007cd52f35826f28e9f7ce775
Rupesh-Rathore <rupesh010903@gmail.com>
2026-05-29 18:03:29 +0530

    Add comment Twosum class

--- JAVA_DSA/Twosum.java
+++ JAVA_DSA/Twosum.java
@@ -5,6 +5,8 @@ import java.util.Scanner;
 import java.util.Arrays;
 import java.util.HashMap;
 
+// leetcode problem - 1
+
 public class Twosum{
     public static void main(String[] args) {
         
