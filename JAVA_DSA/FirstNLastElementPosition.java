d33bc6a0772747eb821af4a08b97ca6e4fac0c9c
Rupesh-Rathore <rupesh010903@gmail.com>
2026-06-09 18:05:33 +0530

    Add FirstNLastElementPosition implementation to find first and last occurrences of a target in a sorted array

---
+++ JAVA_DSA/FirstNLastElementPosition.java
@@ -0,0 +1,62 @@
+package JAVA_DSA;
+
+import java.util.Arrays;
+
+public class FirstNLastElementPosition {
+    public static void main(String[] args) {
+        int[] nums = {5,7,8,8,8,8,10};
+        System.out.println(Arrays.toString(firLasPosSortArr(nums, 8)));
+    }
+
+    static int[] firLasPosSortArr(int nums[], int target){
+
+        int start = 0, end = nums.length-1;
+        int firstPosition = -1,lastPosition = -1;
+
+        // for first occurence - 
+        while (start <= end){
+            int middle = start + (end-start)/2;
+            if (target < nums[middle]){
+                end = middle - 1;
+            }
+            else if (target > nums[middle]){
+                start = middle + 1;
+            }
+            else {
+                // nums[middle] = taget ->
+                if (middle > start && nums[middle-1] == target){
+                    end = middle - 1;
+                }
+                else{
+                    firstPosition = middle;
+                    break;
+                }
+            }
+        }
+
+        // for last occurence
+        start = 0;
+        end = nums.length-1; 
+        while (start <= end) {
+            int middle = start + (end-start)/2;
+            if (target < nums[middle]){
+                end = middle - 1;
+            }
+            else if (target > nums[middle]){
+                start = middle + 1;
+            }
+            else {
+                // nums[middle] = taget ->
+                if (middle < end && nums[middle+1] == target){
+                    start = middle + 1;
+                }
+                else{
+                    lastPosition = middle;
+                    break;
+                }
+            }
+        }
+
+        return(new int[]{firstPosition,lastPosition});
+    }
+}
